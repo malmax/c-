6531c0aa6b7b03ead162ab89cb954e4cd0d54d1a
malmax <malmax.spb@gmail.com>
2016-11-22 17:58:14 +0300

    init hw4

---
+++ tyre2/Lesson03HW/Generic/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Generic
+{
+    class Program
+    {
+        static void Swap<T>(ref T a,ref T b)
+        {
+            T t;
+            t = a;
+            a = b;
+            b = t;
+        }
+
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+            dict.Add("Left", "Влево");
+            dict.Add("Up", "Вверх");
+            if (dict.ContainsKey("login"))
+                if (dict["login"] == "password") ;
+        }
+    }
+}
---
+++ tyre2/Lesson03HW/GenericDelegate/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace GenericDelegate
+{
+    // Этот обобщённый делегат может вызывать любой метод, который возвращает void и принимает 
+    //  единственный параметр типа
+    public delegate void MyGenericDelegate<T>(T arg);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Generic Delegates *****\n");
+            // Зарегистрировать цели.
+            MyGenericDelegate<string> strTarget = new MyGenericDelegate<string>(StringTarget);
+            strTarget("Some string data");
+            MyGenericDelegate<int> intTarget = new MyGenericDelegate<int>(IntTarget);
+            intTarget(9);
+            MyGenericDelegate<double> dblTaget = new MyGenericDelegate<double>(DoubleTarget);
+            dblTaget(10.4);
+            Console.ReadLine();
+        }
+
+        static void StringTarget(string arg)
+        {
+            Console.WriteLine("arg in uppercase is: {0}", arg.ToUpper());
+        }
+
+        static void IntTarget(int arg)
+        {
+            Console.WriteLine("++arg is: {0}", ++arg);
+        }
+
+        static void DoubleTarget(double arg)
+        {
+
+        }
+
+    }
+}
